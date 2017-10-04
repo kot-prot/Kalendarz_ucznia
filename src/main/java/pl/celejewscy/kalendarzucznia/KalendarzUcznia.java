d3aa934c9d01a33f10069024478226da0faa08c4
Filip Celejewski <FilipCelejewski@wp.pl>
2017-10-04 17:50:36 +0200

    Pierwsza działająca wersja

--- src/main/java/pl/celejewscy/kalendarzucznia/KalendarzUcznia.java
+++ src/main/java/pl/celejewscy/kalendarzucznia/KalendarzUcznia.java
@@ -1,7 +1,86 @@
 package pl.celejewscy.kalendarzucznia;
 
-public class KalendarzUcznia {
-    public static void main(String[] args) {
-        System.out.println("A");
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class KalendarzUcznia extends JFrame {
+
+        public static void main(String[] args) {
+            KalendarzUcznia naszaAplikacja = new KalendarzUcznia();
+            naszaAplikacja.setVisible(true);
+        }
+
+        private JTextField data = new JTextField();
+        private JTextArea zadanie = new JTextArea();
+        private JButton przycisk = new JButton("Dodaj zadanie");
+
+        public KalendarzUcznia() {
+            this.setSize(400, 400);
+            this.setLocation(0, 0);
+
+            this.setLayout(new GridBagLayout());
+            this.add(new JLabel("Data:"), new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0, GridBagConstraints.NORTHEAST, GridBagConstraints.NONE, new Insets(1, 1, 1, 1), 1, 1));
+            this.add(data, new GridBagConstraints(1, 0, 1, 1, 1.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL, new Insets(1, 1, 1, 1), 1, 1));
+
+            this.add(new JLabel("Zadanie:"), new GridBagConstraints(0, 1, 1, 1, 0.0, 1.0, GridBagConstraints.NORTHEAST, GridBagConstraints.NONE, new Insets(1, 1, 1, 1), 1, 1));
+            this.add(zadanie, new GridBagConstraints(1, 1, 1, 1, 1.0, 1.0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(1, 1, 1, 1), 1, 1));
+
+            this.add(przycisk, new GridBagConstraints(0, 2, 2, 1, 1.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL, new Insets(1, 1, 1, 1), 1, 1));
+
+            this.addWindowListener(new WindowAdapter() {
+                @Override
+                public void windowClosing(WindowEvent e) {
+                    super.windowClosing(e);
+                    System.exit(0);
+                }
+            });
+
+            ActionListener dodawaczka = new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+
+                    try {
+                        Class.forName("com.mysql.jdbc.Driver");
+
+                        Connection polaczenie =  DriverManager.getConnection("jdbc:mysql://192.168.0.33/kalendarz_ucznia","kalucz", "kacper");
+                        Statement polecenie = polaczenie.createStatement();
+                        polecenie.execute("insert into zadania (id, data, zadanie) values (null, '"+data.getText()+"', '"+zadanie.getText()+"');");
+                        polaczenie.close();
+                        data.setText("");
+                        zadanie.setText("");
+                    } catch (Exception e1) {
+                        e1.printStackTrace();
+                    }
+                }
+            };
+
+            KeyListener obslugiwaczKlawiszyPolaData = new KeyAdapter() {
+                @Override
+                public void keyTyped(KeyEvent e) {
+                    super.keyTyped(e);
+                    if (e.getKeyChar() == KeyEvent.VK_ENTER) {
+                        zadanie.grabFocus();
+                    }
+                }
+            };
+
+            KeyListener obslugiwaczKlawiszyPolaZadanie = new KeyAdapter() {
+                @Override
+                public void keyPressed(KeyEvent e) {
+                    super.keyPressed(e);
+                    if (e.getKeyCode() == KeyEvent.VK_F && e.getModifiers() == 2) {
+                        System.out.println("Teraz zapisz");
+                    }
+                }
+            };
+
+            data.addKeyListener(obslugiwaczKlawiszyPolaData);
+            zadanie.addKeyListener(obslugiwaczKlawiszyPolaZadanie);
+
+            przycisk.addActionListener(dodawaczka);
+        }
     }
-}
